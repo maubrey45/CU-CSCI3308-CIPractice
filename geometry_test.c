7a3c8ef012c62adedb6d0da638fd8547483ec352
user <maau3336@colorado.edu>
2018-11-29 16:35:19 -0700

    Geometry_test is now modified

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,66 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+    double expArea;
+    double testArea;
+
+    //testing if all points are at (0,0), you get a 0 area
+    a.x = 0.0;
+    a.y = 0.0;
+    b.x = 0.0;
+    b.y = 0.0;
+    c.x = 0.0;
+    c.y = 0.0;
+    expArea = 0.0;
+    testArea = coord_2d_area_triangle(&a, &b,&c);
+    //coord_2d_dist(&a, &b) == 0.0
+    ck_assert(testArea == expArea);
+
+    //testing if two points are at (0,0), one is at (2,2), and get zero
+    a.x = 0.0;
+    a.y = 0.0;
+    b.x = 0.0;
+    b.y = 0.0;
+    c.x = 2.0;
+    c.y = 2.0;
+    expArea = 0.0;
+    testArea = coord_2d_area_triangle(&a, &b,&c);
+    //coord_2d_dist(&a, &b) == 0.0
+    ck_assert(testArea == expArea);
+
+    //testing if one point is at (0,0), the rest at none (0,0) you get number
+    //(0,0), (1,2), (1,5)
+    //first establish 
+    a.x = 1.0;
+    a.y = 2.0;
+    b.x = 1.0;
+    b.y = 5.0;
+    c.x = 0.0;
+    c.y = 0.0;
+    expArea = 1.5;
+    testArea = coord_2d_area_triangle(&a, &b,&c);
+    //coord_2d_dist(&a, &b) == 0.0
+    ck_assert(testArea == expArea);
+
+    //testing if all points are at (23,30)(15,15)(42,8), you get 230.5
+    a.x = 23.0;
+    a.y = 30.0;
+    b.x = 15.0;
+    b.y = 15.0;
+    c.x = 42.0;
+    c.y = 8.0;
+    expArea = 230.50;
+    testArea = coord_2d_area_triangle(&a, &b,&c);
+    //coord_2d_dist(&a, &b) == 0.0
+    ck_assert(testArea == expArea);
+}
+END_TEST
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +222,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle,test_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
